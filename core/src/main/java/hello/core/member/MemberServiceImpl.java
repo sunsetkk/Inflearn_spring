8254a20c9cf900656d1ada4e10588ddd720bd48f
sunsetkk <khyejin9796@gmail.com>
2024-12-31 11:46:12 +0900

    MODIFY DIP적용

--- core/src/main/java/hello/core/member/MemberServiceImpl.java
+++ core/src/main/java/hello/core/member/MemberServiceImpl.java
@@ -2,7 +2,14 @@ package hello.core.member;
 
 public class MemberServiceImpl implements MemberService {
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository;
+
+    // 생성자를 통해 외부(AppConfig)에서 구현 객체가 들어올 수 있도록 설계
+    // MemberServiceImpl 은 의존관계에 대한 정보는 AppConfig 에 맡기고 실행에만 집중
+    // MemberServiceImpl 은 MemberRepository 인 추상에만 의존하면 됨
+    public MemberServiceImpl(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
 
     @Override
     public void join(Member member) {
--- core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -9,11 +9,16 @@ import hello.core.member.MemoryMemberRepository;
 
 public class OrderServiceImpl implements OrderService {
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
-    // 할인 정책을 변경하려면 클라이언트 코드를 고쳐야함
-    // 추상인터페이스 뿐 아니라 구체 클래스에도 의존
-    //private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
-    private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
+    private final MemberRepository memberRepository;
+    private final DiscountPolicy discountPolicy;
+
+    // 생성자를 통해 외부(AppConfig)에서 구현 객체가 들어올 수 있도록 설계
+    // OrderServiceImpl 은 의존관계에 대한 정보는 AppConfig 에 맡기고 실행에만 집중
+    // OrderServiceImpl 은 추상에만 의존하면 됨
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
